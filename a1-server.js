26c92aa10a22c05cbd711d34ed5177b33f942a23
RahanGuy <Jakekinahan@gmail.com>
2015-11-09 13:41:02 +0000

    Update a1-server.js

--- a1-server.js
+++ a1-server.js
@@ -21,7 +21,7 @@ var sum;
 // We will use a remote console logging service for debug messages
 // Define the service variable and connnect
 // You must edit <<<change_to_your_repo_name>>> to be your repository name
-var consolere = require('console-remote-client').connect('console.re','80','<<<change_to_your_repo_name>>>');
+var consolere = require('console-remote-client').connect('console.re','80','RahanGuy_Assignment_One');
 
 // Function to handle web browser requests and server responses
 function handleRequest(request, response){
@@ -33,6 +33,7 @@ function handleRequest(request, response){
     // ASSIGNMENT
     // 1. Add a debug message which ouputs the sum of the simple equation above. Values are available in the augend, addend 
     // and sum (global) variables
+    console.re.log(sum);
     // 2. Add a debug message which ouputs the current values of the augend addend and sum variables
 }
 
